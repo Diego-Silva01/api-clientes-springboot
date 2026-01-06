83268edcce87f1f8c5eef8f9c485a90a77ac9ca2
diego <diegoffa01@gmail.com>
2026-01-06 10:55:35 -0300

    validação com DTO

--- APIClentes/src/main/java/com/MinhaAPIclientes/APIClentes/DTO/ClienteDTO.java
+++ APIClentes/src/main/java/com/MinhaAPIclientes/APIClentes/DTO/ClienteDTO.java
@@ -1,8 +1,12 @@
 package com.MinhaAPIclientes.APIClentes.DTO;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 public class ClienteDTO {
-
+    @NotBlank(message = "Nome é obrigatório")
     private String nome;
+    @Email(message = "Email inválido")
+    @NotBlank(message = "Email é obrigatório")
     private String email;
     private String telefone;
     private String endereco;
@@ -13,7 +17,6 @@ public class ClienteDTO {
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
